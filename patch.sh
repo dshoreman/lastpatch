2b1a1230887d282d6f468d67aaa56e852fa76a6b
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-07-27 18:36:16 +0100

    Extract the downloaded XPI into extracted/

--- patch.sh
+++ patch.sh
@@ -2,20 +2,28 @@
 
 pkgname=lastpass
 pkgver=4.29.0.4
+tmpdir="./extracted/"
 
 main() {
     cd "${0%/*}" || exit 1
 
+    prune
     fetch
 
     echo "Done!"
+}
+
+prune() {
+    echo "Pruning old extracted files... "
+    rm -rf ${tmpdir} && mkdir ${tmpdir}
 }
 
 fetch() {
     local filename="in/${pkgname}-${pkgver}.xpi"
 
     echo "Fetching LastPass..."
-    wget --show-progress -qO "${filename}" "https://addons.mozilla.org/firefox/downloads/file/3019318/"
+    wget --show-progress -qO "${filename}" "https://addons.mozilla.org/firefox/downloads/file/3019318/" \
+        && echo "Extracting contents of LastPass XPI file..." && unzip -qqo "${filename}" -d "${tmpdir}"
 }
 
 main
